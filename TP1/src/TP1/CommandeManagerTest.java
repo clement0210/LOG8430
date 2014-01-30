88669b64640e5b004e46adf659cf6e684ae65bef
nevero <rivest.guillaume@gmail.com>
2014-01-30 15:06:36 -0800

    Derniere update aux suites de tests

--- TP1/src/TP1/CommandeManagerTest.java
+++ TP1/src/TP1/CommandeManagerTest.java
@@ -9,16 +9,20 @@ import java.io.File;
 public class CommandeManagerTest {
 
 	private static final String PATH = "C:\\workspace";
-	@SuppressWarnings("unused")
 	private CommandeVide1 cmd1;
+	private CommandeVide2 cmd2;
+	private CommandeVide3 cmd3;
 	
 	@Before
 	public void setUp() throws Exception 
 	{
 		cmd1 = new CommandeVide1( new Invocateur(), new File( PATH ) );
+		cmd2 = new CommandeVide2( new Invocateur(), new File( PATH ) );
+		cmd3 = new CommandeVide3( new Invocateur(), new File( PATH ) );
 	}
 	
 	@Test
+	@Ignore
 	public void testManage()
 	{
 		System.out.println( "\tCommandeManager: testManager()" );
@@ -29,21 +33,21 @@ public class CommandeManagerTest {
 	public void testExecuterCommande1()
 	{
 		System.out.println( "\tCommandeManager: testExecuterCommande1()" );
-		org.junit.Assert.assertTrue( "erreur", true == true  );
+		org.junit.Assert.assertTrue( "erreur", PATH == cmd1.getFichier().getAbsolutePath() );
 	}
 	
 	@Test
 	public void testExecuterCommande2()
 	{
 		System.out.println( "\tCommandeManager: testExecuterCommande2()" );
-		org.junit.Assert.assertTrue( "erreur", true == true  );
+		org.junit.Assert.assertTrue( "erreur", PATH == cmd2.getFichier().getAbsolutePath()  );
 	}
 	
 	@Test
 	public void testExecuterCommande3()
 	{
 		System.out.println( "\tCommandeManager: testExecuterCommande3()" );
-		org.junit.Assert.assertTrue( "erreur", true == true  );
+		org.junit.Assert.assertTrue( "erreur", PATH == cmd3.getFichier().getAbsolutePath()  );
 	}
 	
 	@Test
