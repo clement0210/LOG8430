4205fcbc97acc3543604d1c694878808b506ccdb
nevero <nevero@nevero-MBPro.(none)>
2014-02-12 09:47:37 -0500

    Test du chargement dynamique

--- TP2/src/TP2/test/CommandeManagerTest.java
+++ TP2/src/TP2/test/CommandeManagerTest.java
@@ -4,12 +4,14 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import TP2.CommandeDynamic;
 import TP2.CommandeVide1;
 import TP2.CommandeVide2;
 import TP2.CommandeVide3;
 import TP2.Invocateur;
 
 import java.io.File;
+import java.util.Map;
 
 public class CommandeManagerTest {
 
@@ -17,49 +19,28 @@ public class CommandeManagerTest {
 	private CommandeVide1 cmd1;
 	private CommandeVide2 cmd2;
 	private CommandeVide3 cmd3;
+	private Map<Integer, CommandeDynamic> commandeFichier;
 	
 	@Before
 	public void setUp() throws Exception 
 	{
-		cmd1 = new CommandeVide1( new Invocateur(), new File( PATH ) );
-		cmd2 = new CommandeVide2( new Invocateur(), new File( PATH ) );
-		cmd3 = new CommandeVide3( new Invocateur(), new File( PATH ) );
+		testFillCommandeFichier( commandeFichier );
 	}
 	
 	@Test
 	@Ignore
-	public void testManage()
+	public void testFillCommandeFichier( Map<Integer, CommandeDynamic> commandeFichier ) throws ClassNotFoundException
 	{
-		System.out.println( "\tCommandeManager: testManager()" );
-		org.junit.Assert.assertTrue( "erreur", true == true  );
+		Class c1 = Class.forName( "TestSuite1.class" );
+		Object obj1 = null;
+		CommandeDynamic cmdDyn1 = new CommandeDynamic( c1, obj1 );
+		commandeFichier.put( 7, cmdDyn1 );
 	}
 	
 	@Test
-	public void testExecuterCommande1()
+	public void testExecuterCommande( int id )
 	{
-		System.out.println( "\tCommandeManager: testExecuterCommande1()" );
-		org.junit.Assert.assertTrue( "erreur", PATH == cmd1.getFichier().getAbsolutePath() );
-	}
-	
-	@Test
-	public void testExecuterCommande2()
-	{
-		System.out.println( "\tCommandeManager: testExecuterCommande2()" );
-		org.junit.Assert.assertTrue( "erreur", PATH == cmd2.getFichier().getAbsolutePath()  );
-	}
-	
-	@Test
-	public void testExecuterCommande3()
-	{
-		System.out.println( "\tCommandeManager: testExecuterCommande3()" );
-		org.junit.Assert.assertTrue( "erreur", PATH == cmd3.getFichier().getAbsolutePath()  );
-	}
-	
-	@Test
-	@Ignore
-	public void testExecuterSelect()
-	{
-		System.out.println( "\tCommandeManager: testExecuterSelect()" );
-		org.junit.Assert.assertTrue( "erreur", true == true  );
+		System.out.println( "\tCommandeManager: testExecuterCommande" );
+		org.junit.Assert.assertTrue( "CommandeManager: Classe mal chargée", commandeFichier.get( 7 ).getClasseCommande().toString() == "TestSuite1.class" );
 	}
 }
